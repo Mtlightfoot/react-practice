ba0382f23bf020a598a1f98fe5803214c57c9da1
Matt Lightfoot <m-lightfoot@hotmail.com>
2024-08-13 16:51:09 +0100

    Added random colour generator to the front page title

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function HomeComponent() {
-  return (
-    <h1 className='welcomeText'>Welcome</h1>
+
+  const [colour, setColour] = useState("#acacac")
+
+  const style = { backgroundColor: colour }
+
+  const randomColour = Math.floor(Math.random() * 16777215).toString(16);
+
+  function changeColour() {
+    setColour(`#${randomColour}`)
+  }
+
+  return (<>
+    <button onClick={changeColour} id='welcomeBtn'><h1 style={style} className='welcomeText'>Welcome</h1></button>
+    <h5>Click to change colour</h5>
+  </>
   )
 }
 
